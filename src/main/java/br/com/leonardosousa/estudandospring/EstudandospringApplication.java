04d28d4943c4d18216bdea493abae1715865a103
leonardo.sousa <leonardo.sousa@cabal.com.br>
2020-02-26 18:00:57 -0300

    Aulas

--- src/main/java/br/com/leonardosousa/estudandospring/EstudandospringApplication.java
+++ src/main/java/br/com/leonardosousa/estudandospring/EstudandospringApplication.java
@@ -1,13 +1,21 @@
 package br.com.leonardosousa.estudandospring;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EstudandospringApplication {
+
+	@Value("teste123")
+	private Long teste123;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudandospringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner
 }
