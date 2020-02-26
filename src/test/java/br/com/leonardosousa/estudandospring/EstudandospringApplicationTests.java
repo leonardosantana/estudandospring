b08077b467e273436ff65e93013a38fff254b830
leonardo.sousa <leonardo.sousa@cabal.com.br>
2020-02-26 16:29:15 -0300

    criando repositorio

---
+++ src/test/java/br/com/leonardosousa/estudandospring/EstudandospringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardosousa.estudandospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
